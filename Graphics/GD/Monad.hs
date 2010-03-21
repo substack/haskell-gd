2c14a8efbb54eb1c5a1de2733881063001797085
James Halliday <substack@gmail.com>
2010-03-20 16:18:24 -0800

    GD is the new GDUpdate

--- Graphics/GD/Monad.hs
+++ Graphics/GD/Monad.hs
@@ -1,5 +1,5 @@
 module Graphics.GD.Monad (
-    withImage, setPixel
+    GD, withImage, newImage, setPixel
 ) where
 
 import qualified Graphics.GD as GD
@@ -7,28 +7,28 @@ import Control.Monad.State.Lazy
 import Control.Applicative ((<$>))
 import Control.Monad (mapM_)
 
-data GD = GD { gdCommands :: [GDCommand], gdImage :: GD.Image }
+data GD' = GD' { gdCommands :: [GDCommand], gdImage :: GD.Image }
 
 data GDCommand
     = CopyImage
     | SetPixel GD.Point GD.Color
 
-type GDState a = State GD a
-type GDUpdate = GDState ()
+type GDState a = State GD' a
+type GD = GDState ()
 
-withImage :: GD.Image -> GDUpdate -> IO GD.Image
+withImage :: GD.Image -> GD -> IO GD.Image
 withImage im f = do
     im' <- GD.copyImage im
-    mapM_ (flip runCmd $ im') $ gdCommands $ execState f (GD [] im')
+    mapM_ (flip runCmd $ im') $ gdCommands $ execState f (GD' [] im')
     return im'
 
-newImage :: GD.Size -> GDUpdate -> IO GD.Image
+newImage :: GD.Size -> GD -> IO GD.Image
 newImage size f = ($ f) <$> withImage =<< GD.newImage size
 
-setPixel :: GD.Point -> GD.Color -> GDUpdate
+setPixel :: GD.Point -> GD.Color -> GD
 setPixel point color = consCmd $ SetPixel point color
 
-consCmd :: GDCommand -> GDUpdate
+consCmd :: GDCommand -> GD
 consCmd cmd = modify $ \gd -> gd { gdCommands = cmd : (gdCommands gd) }
 
 runCmd :: GDCommand -> GD.Image -> IO ()
