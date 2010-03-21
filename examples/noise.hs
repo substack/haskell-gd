582bd57126a2e72f6b6e971ae116815b060a88dd
James Halliday <substack@gmail.com>
2010-03-21 01:25:19 -0800

    256, not 255

--- examples/noise.hs
+++ examples/noise.hs
@@ -4,4 +4,4 @@ import System.Random
 main = do
     g <- newStdGen
     (savePngFile "noise.png" =<<) . newImage (400,300) $ do
-        setPixels $ map fromInteger $ randomRs (0,255^3) g
+        setPixels $ map fromInteger $ randomRs (0,256^3-1) g
